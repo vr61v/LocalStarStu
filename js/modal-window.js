b72a65cc5ea5d1f8c6e0b6bf476914e6e8bcae5c
Vr61v <nikita20040503@gmail.com>
2023-07-17 18:57:45 +0300

    Hot Fix 320px media + create robots + sitemap

--- js/modal-window.js
+++ js/modal-window.js
@@ -1,14 +1,17 @@
 var modal_calc = document.getElementById("calc");
 var button_calc = document.getElementById("button-calc");
 var span_calc = document.getElementsByClassName("modal-calc__close")[0];
+var body_html = document.getElementById("body");
 
 
 button_calc.onclick = function() {
   console.log(modal_calc)
   modal_calc.classList.add('modal-calc__open')
+  body_html.classList.add('body__modal-open')
 }
 span_calc.onclick = function() {
   modal_calc.classList.remove('modal-calc__open')
+  body_html.classList.remove('body__modal-open')
 }
 
 
@@ -20,9 +23,11 @@ var span_rules = document.getElementsByClassName("modal-rules__close")[0];
 button_rules.onclick = function() {
   console.log(modal_rules)
   modal_rules.classList.add('modal-rules__open')
+  body_html.classList.add('body__modal-open')
 }
 span_rules.onclick = function() {
   modal_rules.classList.remove('modal-rules__open')
+  body_html.classList.remove('body__modal-open')
 }
 
 
@@ -30,8 +35,10 @@ span_rules.onclick = function() {
 window.onclick = function(event) {
   if (event.target == modal_calc) {
     modal_calc.classList.remove('modal-calc__open')
+    body_html.classList.remove('body__modal-open')
   }
   if (event.target == modal_rules) {
     modal_rules.classList.remove('modal-rules__open')
+    body_html.classList.remove('body__modal-open')
   }
 }
